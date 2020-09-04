a9d60cbad87b30f69f30862f62756678c4e61c6c
Trevor Davis <trevor_davis@sfu.ca>
2020-09-04 13:36:01 -0700

    New API

--- apiv1.php
+++ apiv1.php
@@ -5,24 +5,25 @@
 		//global $configInfo; 
 		global $db;
 		$room_id=$_GET['room_id'];
-		$sql="SELECT * from safety_plans WHERE room_id=$room_id";
+		$sql="SELECT * FROM safety_plans LEFT JOIN user_room ON(safety_plans.room_id=user_room.id) WHERE safety_plans.room_id=$room_id";
 		$plan=$db->getRow($sql);
 		if($plan){
-			response(0,$plan['pi'],$plan['contact'],$plan['purpose'],$plan['occupancy'],$plan['plan']);
+			response(0,$plan['pi'],$plan['contact'],$plan['purpose'],$plan['occupancy'],$plan['plan'],$plan['name']);
 		} else {
-			response(200,NULL,NULL,NULL,NULL,"No Record Found");
+			response(200,NULL,NULL,NULL,NULL,"No Record Found",NULL);
 		}
 	} else {
-		response(400,NULL,NULL,NULL,NULL,"Invalid Request");
+		response(400,NULL,NULL,NULL,NULL,"Invalid Request",NULL);
 	}
 	
-	function response($response_code,$pi,$contact,$purpose,$occupany,$plan){
+	function response($response_code,$pi,$contact,$purpose,$occupany,$plan,$name){
 		$response['pi'] = $pi;
 		$response['contact'] = $contact;
 		$response['response_code'] = $response_code;
 		$response['purpose'] = $purpose;
 		$response['occupany'] = $occupany;
 		$response['plan'] = $plan;
+		$response['name'] = $name;
  
 		$json_response = json_encode($response);
 		echo $json_response;
